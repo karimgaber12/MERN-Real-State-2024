f4034cff9323f620e3165b31631ca1a1a336916d
karimgaber12 <karimgaberhd@gmail.com>
2024-07-10 17:48:36 +0300

    complete signup functionality frontend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
                 <h1 className='font-bold text-small-sm sm:text-xl flex flex-wrap'>
-                    <span className='text-slate-500'>KARIM</span>
+                    <span className='text-slate-500'>Real</span>
                     <span className="text-slate-700">Estate</span>
                 </h1>
             </Link>
